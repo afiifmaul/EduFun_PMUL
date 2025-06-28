b828bce81cc305000ad2cc806e104e9e0978c126
ario-create <ario3533@student.gunadarma.ac.id>
2025-06-28 22:06:50 +0700

    add math, Indonesian, and science questions

--- src/app/data/quizzes.js
+++ src/app/data/quizzes.js
@@ -11,9 +11,39 @@ export const quizzes = {
       },
       {
         id: 2,
-        type: "essay",
-        question: "Jelaskan apa itu teorema Pythagoras!",
+        type: "mcq",
+        question: "Berapakah hasil dari 15 + 8?",
+        options: ["22", "23", "24", "25"],
+        answer: "23",
       },
+      {
+        id: 3,
+        type: "mcq",
+        question: "Berapakah 30 dibagi 5?",
+        options: ["5", "6", "7", "8"],
+        answer: "6",
+      },
+      {
+        id: 4,
+        type: "mcq",
+        question: "Bilangan genap di bawah ini adalah?",
+        options: ["5", "7", "8", "9"],
+        answer: "8",
+      },
+      {
+        id: 5,
+        type: "mcq",
+        question: "Sudut siku-siku besarnya adalah?",
+        options: ["45 derajat", "90 derajat", "180 derajat", "360 derajat"],
+        answer: "90 derajat",
+      },
+      {
+        id: 6,
+        type: "mcq",
+        question: "Berapakah hasil dari 12 − 4?",
+        options: ["6", "7", "8", "9"],
+        answer: "8",
+      }
     ],
   },
   bahasa: {
@@ -28,10 +58,86 @@ export const quizzes = {
       },
       {
         id: 2,
-        type: "essay",
-        question:
-          "Tuliskan sebuah paragraf singkat tentang kebudayaan Indonesia.",
+        type: "mcq",
+        question: "Sinonim dari kata 'besar' adalah?",
+        options: ["tinggi", "panjang", "luas", "raksasa"],
+        answer: "raksasa",
       },
+      {
+        id: 3,
+        type: "mcq",
+        question: "Kata sapaan untuk guru adalah?",
+        options: ["Bu Guru", "Adik", "Kakak", "Om"],
+        answer: "Bu Guru",
+      },
+      {
+        id: 4,
+        type: "mcq",
+        question: "Antonim dari kata 'panjang' adalah?",
+        options: ["tinggi", "besar", "pendek", "kecil"],
+        answer: "pendek",
+      },
+      {
+        id: 5,
+        type: "mcq",
+        question: "Kalimat tanya biasanya diakhiri dengan tanda?",
+        options: ["titik", "koma", "seru", "tanya"],
+        answer: "tanya",
+      },
+      {
+        id: 6,
+        type: "mcq",
+        question: "Hewan dalam dongeng sering bisa?",
+        options: ["berjalan", "bicara", "terbang", "menyanyi"],
+        answer: "bicara",
+      }
     ],
   },
+  ipa: {
+    title: "Kuis IPA",
+    questions: [
+      {
+        id: 1,
+        type: "mcq",
+        question: "Bagian tubuh yang digunakan untuk melihat adalah?",
+        options: ["telinga", "mata", "hidung", "kaki"],
+        answer: "mata",
+      },
+      {
+        id: 2,
+        type: "mcq",
+        question: "Matahari terbit dari arah?",
+        options: ["utara", "selatan", "timur", "barat"],
+        answer: "timur",
+      },
+      {
+        id: 3,
+        type: "mcq",
+        question: "Air berubah menjadi uap jika?",
+        options: ["dibekukan", "direbus", "didiamkan", "didinginkan"],
+        answer: "direbus",
+      },
+      {
+        id: 4,
+        type: "mcq",
+        question: "Tumbuhan membutuhkan ... untuk membuat makanan?",
+        options: ["air", "garam", "batu", "minyak"],
+        answer: "air",
+      },
+      {
+        id: 5,
+        type: "mcq",
+        question: "Contoh hewan yang hidup di air adalah?",
+        options: ["kucing", "ayam", "ikan", "kuda"],
+        answer: "ikan",
+      },
+      {
+        id: 6,
+        type: "mcq",
+        question: "Manusia bernapas menggunakan?",
+        options: ["kulit", "mata", "paru-paru", "telinga"],
+        answer: "paru-paru",
+      }
+    ],
+  }
 };
