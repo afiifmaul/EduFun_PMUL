d24b99674a86afab448424a7c49cef643be52ec4
Maula <maulanajmipn@gmail.com>
2025-07-09 21:43:30 +0700

    style(quiz): update the card

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -4,7 +4,7 @@ export default function QuizPage() {
   return (
     <div className="space-y-6">
       <h1 className="text-2xl font-bold">Kuis Tersedia</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-6xl">
         {courses.map((c) => (
           <QuizCard
             key={c.id}
